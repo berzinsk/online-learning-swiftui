87c15b341b790b70a1b2908a06a3a3bf71058206
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-02-28 06:39:09 +0200

    Create UI for home header view

---
+++ online-learning-ui/Screens/Home/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  online-learning-ui
+//
+//  Created by Karlis Berzins on 28/02/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            HomeHeaderView()
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
