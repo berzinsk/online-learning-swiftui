d04a19edea5329a70e3bf748fa2cc4d32f2be82e
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-03-07 20:11:41 +0200

    Add apple default tabbar to app for navigation

---
+++ online-learning-ui/Screens/Message/MessageView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageView.swift
+//  online-learning-ui
+//
+//  Created by Karlis Berzins on 07/03/2023.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
--- online-learning-ui/online_learning_uiApp.swift
+++ online-learning-ui/online_learning_uiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct online_learning_uiApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            AppView()
         }
     }
 }
